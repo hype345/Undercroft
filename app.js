2464df3fdba63004a1d46d9b2f25aa7292bb9ded
Chase Shriver <chase802@icloud.com>
2020-05-07 12:40:31 -0400

    basic messge feature

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser= require('body-parser') 
 const path = require('path');
 const routes = require('./routes/index.js');
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+
 
 
 const app = express();
@@ -40,52 +41,37 @@ mongoUtil.connectToServer( function( err, client ) {
 
     var db = mongoUtil.getDb();
     // add mongo code here
-    app.post('/send', (req, res)=>{
-      console.log(req.body)
-      // const output=`
-      //     <p>You have a new contact request</p>
-      //     <h3>Contact Details</h3>
-      //     <ul>  
-      //       <li>Name: ${req.body.name}</li>
-      //       <li>Email: ${req.body.email}</li>
-      //     </ul>
 
-      //     <h3>Message</h3>
-      //     <p>${req.body.message}</p>
-      //   `;
-      const output=`<p>hello</p>`;
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-          host: 'mail.google.com',
-          port: 587,
-          secure: false, // true for 465, false for other ports
-          auth: {
-              user: 'jgbad8@gmail.com', // generated ethereal user
-              pass: 'shebab99'  // generated ethereal password
-          },
-          tls:{
-            rejectUnauthorized:false
-          }
-        });
 
-    // setup email data with unicode symbols
-      let mailOptions = {
-          from: '"Nodemailer Contact" <jgbad8@gmail.com>', // sender address
-          to: 'jgbad007@gmail.com', // list of receivers
-          subject: 'Node Contact Request', // Subject line
-          text: 'Hello world?', // plain text body
-          html: 'output' // html body
-        };
+    app.post('/send', (req, res) => {
 
-      // send mail with defined transport object
-      transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);   
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        //hidden code
+        auth: {
+          //hidden code
+              user: 'cocminioncoc@gmail.com',
+              pass: 'ClashofClans83*' 
+        }
+      });
 
-            res.render('aboutus', {msg:'Email has been sent'});
-        });
+      var mailOptions = {
+        from: 'cocminioncoc@gmail.com',
+        to: 'cshriver@friendsbalt.org',
+        subject: 'Sending Email using Node.js',
+        text: req.body.message
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+        res.render('aboutus');
+      });
     })
+
+
+
   })
