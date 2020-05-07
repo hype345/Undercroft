005cc482b96dcf8cad68adba9c59db1f48c5db10
Joseph Badros <jbadros@friendsbalt.org>
2020-05-07 11:30:29 -0400

    basis of the nodemailer, go back here if all fails

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser= require('body-parser') 
 const path = require('path');
 const routes = require('./routes/index.js');
+const exphbs = require('express-handlebars');
+const nodemailer = require('nodemailer')
 
 
 const app = express();
+
 // Set the default views directory to html folder
 app.set('views', path.join(__dirname, 'html'));
+
+
 
 // Set the folder for css & java scripts
 app.use(express.static(path.join(__dirname,'css')));
@@ -25,20 +30,63 @@ app.set('view engine', 'ejs');
 
 
 
-
-
 var mongoUtil = require('./routes/mongoUtil.js');
-
 mongoUtil.connectToServer( function( err, client ) {
-  app.use('/', routes); 
+    app.use('/', routes); 
 
-  if (err) console.log(err);
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running at localhost:3000');
-  });
+    if (err) console.log(err);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Server is running at localhost:3000');
+    });
 
-  var db = mongoUtil.getDb();
+    var db = mongoUtil.getDb();
+    // add mongo code here
+    app.post('/send', (req, res)=>{
+      console.log(req.body)
+      // const output=`
+      //     <p>You have a new contact request</p>
+      //     <h3>Contact Details</h3>
+      //     <ul>  
+      //       <li>Name: ${req.body.name}</li>
+      //       <li>Email: ${req.body.email}</li>
+      //     </ul>
 
-  //add mongo code here
+      //     <h3>Message</h3>
+      //     <p>${req.body.message}</p>
+      //   `;
+      const output=`<p>hello</p>`;
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+          host: 'mail.google.com',
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+              user: 'jgbad8@gmail.com', // generated ethereal user
+              pass: 'shebab99'  // generated ethereal password
+          },
+          tls:{
+            rejectUnauthorized:false
+          }
+        });
 
-} );
+    // setup email data with unicode symbols
+      let mailOptions = {
+          from: '"Nodemailer Contact" <jgbad8@gmail.com>', // sender address
+          to: 'jgbad007@gmail.com', // list of receivers
+          subject: 'Node Contact Request', // Subject line
+          text: 'Hello world?', // plain text body
+          html: 'output' // html body
+        };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);   
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            res.render('aboutus', {msg:'Email has been sent'});
+        });
+    })
+  })
