00a5188bb9646f3c09b8252c64a452e1a6996282
Chase Shriver <chase802@icloud.com>
2020-05-11 17:59:53 -0400

    small changes

--- javascripts/googlecaleandar.js
+++ javascripts/googlecaleandar.js
@@ -1,35 +1,8 @@
 
-  var userEvents = [
-    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
-  ];
-  var settings = {};
-  var element = document.getElementById('caleandar');
-  caleandar(element, userEvents, settings);
+  const googlecalendar = require('../app.js')
+  
+  var calendar = googlecalendar.getCalendar();
 
-
-
-
-
-
-
-
-
-const { google } = require("googleapis")
-
-const { OAuth2 } = google.auth
-
-const oAuth2Client = new OAuth2('574314752299-uuqgd8bahgh7nkb3qvu64vh763t0omra.apps.googleusercontent.com', 
-'c0j7mUcCRK2wRVjTrNHttPvG'
-)
-
-oAuth2Client.setCredentials(
-    {refresh_token: 
-    '1//04YrnyJK0WStHCgYIARAAGAQSNwF-L9IrfIDQcAumg65lQSs2OllL1bTd0r_wdzHh_sQAO_ZOSBXOZ3N-DAc8chtBDCT00xlC1g0',
-})
-
-const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
 
 calendar.events.list({
     calendarId: 'primary',
@@ -50,4 +23,15 @@ calendar.events.list({
       console.log('No upcoming events found.');
     }
   });
+
+
+  
+  var userEvents = [
+    {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
+    {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
+    {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+  ];
+  var settings = {};
+  var element = document.getElementById('caleandar');
+  caleandar(element, userEvents, settings);
 
