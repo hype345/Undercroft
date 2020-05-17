bfb4f632af70e6526c8f2c491e034215da456b06
Chase Shriver <chase802@icloud.com>
2020-05-17 15:11:35 -0400

    env

--- routes/googleCalendarUtil.js
+++ routes/googleCalendarUtil.js
@@ -1,26 +1,15 @@
-
-//OAuth conection
-// const { google } = require("googleapis")
-// const { OAuth2 } = google.auth
-// const oAuth2Client = new OAuth2('574314752299-uuqgd8bahgh7nkb3qvu64vh763t0omra.apps.googleusercontent.com', 
-// 'c0j7mUcCRK2wRVjTrNHttPvG'
-// )
-// oAuth2Client.setCredentials(
-//     {refresh_token: 
-//     '1//04YrnyJK0WStHCgYIARAAGAQSNwF-L9IrfIDQcAumg65lQSs2OllL1bTd0r_wdzHh_sQAO_ZOSBXOZ3N-DAc8chtBDCT00xlC1g0',
-// })
-
-// const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+require('dotenv').config();
 
 const { google } = require("googleapis")
-let privatekey = require("../privatekey.json");
+let client_email = process.env.client_email;
+let private_key = process.env.private_key;
 
 
 // configure a JWT auth client
 let jwtClient = new google.auth.JWT(
-  privatekey.client_email,
+  client_email,
   null,
-  privatekey.private_key,
+  private_key,
   ['https://www.googleapis.com/auth/calendar']);
 //authenticate request
 jwtClient.authorize(function (err, tokens) {
