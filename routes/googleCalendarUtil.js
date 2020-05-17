4e2629421a4d0910e1ac4c221b7ddcfed3701967
Chase Shriver <chase802@icloud.com>
2020-05-17 14:27:08 -0400

    service account

--- routes/googleCalendarUtil.js
+++ routes/googleCalendarUtil.js
@@ -1,19 +1,43 @@
 
-//google calendar
+//OAuth conection
+// const { google } = require("googleapis")
+// const { OAuth2 } = google.auth
+// const oAuth2Client = new OAuth2('574314752299-uuqgd8bahgh7nkb3qvu64vh763t0omra.apps.googleusercontent.com', 
+// 'c0j7mUcCRK2wRVjTrNHttPvG'
+// )
+// oAuth2Client.setCredentials(
+//     {refresh_token: 
+//     '1//04YrnyJK0WStHCgYIARAAGAQSNwF-L9IrfIDQcAumg65lQSs2OllL1bTd0r_wdzHh_sQAO_ZOSBXOZ3N-DAc8chtBDCT00xlC1g0',
+// })
+
+// const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+
 const { google } = require("googleapis")
-const { OAuth2 } = google.auth
-const oAuth2Client = new OAuth2('574314752299-uuqgd8bahgh7nkb3qvu64vh763t0omra.apps.googleusercontent.com', 
-'c0j7mUcCRK2wRVjTrNHttPvG'
-)
-oAuth2Client.setCredentials(
-    {refresh_token: 
-    '1//04YrnyJK0WStHCgYIARAAGAQSNwF-L9IrfIDQcAumg65lQSs2OllL1bTd0r_wdzHh_sQAO_ZOSBXOZ3N-DAc8chtBDCT00xlC1g0',
-})
-const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+let privatekey = require("../privatekey.json");
+
+
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/calendar']);
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+if (err) {
+console.log(err);
+return;
+} else {
+console.log("Successfully connected!");
+}
+});
+
+let calendar = google.calendar('v3');
 
 let userEvents = function() {
   return calendar.events.list({
-    calendarId: 'primary',
+    auth: jwtClient,
+    calendarId: 'cocminioncoc@gmail.com',
     timeMin: (new Date(-8640000000000000)).toISOString(),
     singleEvents: true,
     orderBy: 'startTime',
@@ -22,7 +46,8 @@ let userEvents = function() {
 
 let findEventById = function(eventid) {
 return calendar.events.get({
-calendarId: 'primary',
+auth: jwtClient,
+calendarId: 'cocminioncoc@gmail.com',
 eventId: eventid,
 }).then(userData => { return userData.data} )
 }
